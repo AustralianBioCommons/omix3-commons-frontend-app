d1bb8678727a197bffbd0e7c369bc203623f7d94
NagasriAlava-bioc <nagasri@biocommons.org.au>
2026-03-22 18:38:46 +1100

    updated dictionary file

--- src/pages/DataDictionary.tsx
+++ src/pages/DataDictionary.tsx
@@ -1,7 +1,28 @@
-import {
-  DataDictionaryPage,
-  DictionaryPageGetServerSideProps as getServerSideProps,
-} from '@gen3/frontend';
+import { NavPageLayout, DictionaryPageGetServerSideProps as getServerSideProps } from '@gen3/frontend';
+import type { DictionaryConfig, NavPageLayoutProps } from '@gen3/frontend';
+import DictionaryWithContext from '@/components/DataDictionary/DictionaryWithContext';
+
+type DataDictionaryPageProps = NavPageLayoutProps & {
+  config: DictionaryConfig;
+};
+
+const DataDictionaryPage = ({ headerProps, footerProps, config }: DataDictionaryPageProps) => {
+  return (
+    <NavPageLayout
+      headerProps={headerProps}
+      footerProps={footerProps}
+      mainProps={{ fixed: true }}
+      headerMetadata={{
+        title: 'Gen3 DataDictionary Page',
+        content: 'Data Dictionary',
+        key: 'gen3-data-dictionary-page',
+        ...(config?.headerMetadata ? config.headerMetadata : {}),
+      }}
+    >
+      <DictionaryWithContext config={config} />
+    </NavPageLayout>
+  );
+};
 
 export default DataDictionaryPage;
 
